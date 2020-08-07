07ea97b5015f39df20320d8b574896d256037fc7
Akumu collins <akumucollins001@gmail.com>
2020-08-07 10:01:42 +0300

    corrected the jquery function and the search button structure

--- js/script.js
+++ js/script.js
@@ -22,9 +22,10 @@ $(document).ready(function () {
         console.log(response);
 
         // Loop through all the items one-by-one
-        for (var i = 0; i < response.items.length; i++) {
+        response.items.map((book) => {
+
           // set the items from the response object
-          var items = response.items[i];
+          var items = book;
           image = items.volumeInfo.imageLinks.thumbnail;
           title = items.volumeInfo.title;
           author = items.volumeInfo.authors;
@@ -38,41 +39,41 @@ $(document).ready(function () {
 
           // Set the book details in the div
           document.getElementById("result").innerHTML += `
-            <div class="container text-center" id="book">
-              <div id="image">
-                <img src=${image} width="100px" alt="book cover " />
+            <div class="container-fluid text-center" id="book">
+              <div>
+                <img src=${image} width="100px" alt="book cover "/>
               </div>
-              <div id="title">
-                <h3 className="inline">Book Title: </h3> ${title}
+              <div>
+                <h3>Book Title: </h3>${title}
               </div>
-              <div id="author">
-                <h3 className="inline">Author:</h3>${author}
+              <div>
+                <h3>Author:</h3>${author}
               </div>
-              <div id="publisher">
-                <h3 className="inline">Publisher:</h3>${publisher}
+              <div>
+                <h3>Publisher:</h3>${publisher}
               </div>
-              <div id="published">
-                <h3 className="inline">Originally Published On:</h3>${publishedDate}
+              <div>
+                <h3>Originally Published On:</h3>${publishedDate}
               </div>
-              <div id="description">
-                <h3 className="inline">Description:</h3>${description}
+              <div>
+                <h3>Description:</h3>${description}
               </div>
               <div id="pages">
-                <h3 className="inline">Number of Pages:</h3>${pageCount}
+                <h3>Number of Pages:</h3>${pageCount}
               </div>
-              <div id="categories">
+              <div>
                 <h3>Category: </h3>${categories}
               </div><br>
               <div id="preview">
-              <button class="btn"><a href="${preview}">Preview</a></button>
+                <button class="btn"><a href="${preview}">Preview</a></button>
               </div><br>
               <div id="info">
-              <button class="btn"><a href="${url}">Read More</a></button>
+                <button class="btn"><a href="${url}">Read More</a></button>
               </div>
             </div>
-          </div>  
           `;
-        }
+        })
+
       });
     }
     return false;
